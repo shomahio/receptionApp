14387d77793b894e4cf0bd57a2483708e3b4fe9f
shomahio <75551441+shomahio@users.noreply.github.com>
2021-02-27 16:57:52 +0900

    一般ユーザー管理機能

--- app/models/store.rb
+++ app/models/store.rb
@@ -4,17 +4,7 @@ class Store < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         with_options presence: true do
-           validates :store_name
-           validates :postal_code,       format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "にハイフンを含めてください" }
-           validates :prefecture_id,     numericality: {other_than: 1, message:'を選択してください'}
-           validates :city
-           validates :address
-           validates :director
-           validates :tel,               format: { with: /\A\d{10,11}\z/}
-           validates :email
-           validates :password,          format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, length: { minimum: 6 }, message: '半角文字で英数どちらも最低１文字は使用してください' }
-         end
+
   
   has_many :informations, dependent: :destroy
 
