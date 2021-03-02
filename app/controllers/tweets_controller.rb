2ce344a6bbc403b9b692ae70af29f938b0e0e966
shomahio <75551441+shomahio@users.noreply.github.com>
2021-03-02 16:45:44 +0900

    ツイートの編集・削除・詳細機能

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -13,6 +13,24 @@ class TweetsController < ApplicationController
     end
   end
 
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+  end
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+
   private
   def tweet_params
     params.require(:tweet).permit(:name, :text, :image).merge(user_id: current_user.id)
