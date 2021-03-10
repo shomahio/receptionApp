cab164fb5b798220580acbac5e4b9f5d344a32e8
shomahio <75551441+shomahio@users.noreply.github.com>
2021-03-10 20:52:59 +0900

    予約カレンダーのJSの実装

--- app/javascript/reservation.js
+++ app/javascript/reservation.js
@@ -5,10 +5,32 @@ function check() {
   days.forEach(function (day) { 
     day.addEventListener("click", () => {
       const hiddenClass = day.getAttribute("class");
-      hiddenClass.removeAttribute("style","display; none;")
-      console.log(hiddenClass);
-      console.log(day);
+      let children = day.childNodes;
+      const children2 = children[3];
+      if (children2.getAttribute("style") == "display:none;") {
+        children2.removeAttribute("style", "display:none;")
+      }
+    });
+
+    day.addEventListener("mouseout", () => {
+      const hiddenClass = day.getAttribute("class");
+      let children = day.childNodes;
+      const children2 = children[3];
+      if (children2.getAttribute("style") != "display:none;") {
+        children2.setAttribute("style", "display:none;")
+      }
     });
   });
 }
 window.addEventListener("load", check)
+
+
+//pullDownButton.addEventListener('click', function() {
+  //debugger
+  // プルダウンメニューの表示と非表示の設定
+  //if (pullDownParents.getAttribute("style") == "display:block;") {
+    //pullDownParents.removeAttribute("style", "display:block;")
+  //} else {
+    //pullDownParents.setAttribute("style", "display:block;")
+  //}
+//})
