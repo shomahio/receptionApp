1efe07bf2dc449da4fbd3a2f78120f9d0d10d195
shomahio <75551441+shomahio@users.noreply.github.com>
2021-04-05 19:37:16 +0900

    Information.indexのフロント実装

--- app/javascript/search.js
+++ app/javascript/search.js
@@ -1,27 +1,43 @@
 window.addEventListener('load', function(){
+  const genrePictures = document.querySelector(".genre_btn")
+  const placeBtns = document.querySelector(".place_btn")
+  const detailBtns = document.querySelectorAll(".detail_tag")
 
-  const pullDownRadioButton = document.getElementById("search_genre1")
-  const pullDownRadioButtons = document.getElementById("genre_btn1")
-  const pullDownRadioButton1 = document.getElementById("search_place1")
-  const pullDownRadioButtons1 = document.getElementById("place_btn1")
-
-  pullDownRadioButton.addEventListener('click', function(){
-    pullDownRadioButtons.removeAttribute("style", "display:none;")
-  })
-
-  pullDownRadioButtons.addEventListener("mouseleave", () => {
-    if (pullDownRadioButtons.getAttribute("style") != "display:none;") {
-      pullDownRadioButtons.setAttribute("style", "display:none;")
-    }
+  const genrePicture = genrePictures.childNodes
+  genrePicture.forEach(function (picture) { 
+    picture.addEventListener ("click", () => {
+      if (picture.getAttribute("style") == "filter: opacity(40%);") {
+        picture.removeAttribute("style", "filter: opacity(40%);")
+      }else {
+        picture.setAttribute("style", "filter: opacity(40%);")
+      }
+    });
   });
 
-  pullDownRadioButton1.addEventListener('click', function(){
-    pullDownRadioButtons1.removeAttribute("style", "display:none;")
-  })
-
-  pullDownRadioButtons1.addEventListener("mouseleave", () => {
-    if (pullDownRadioButtons1.getAttribute("style") != "display:none;") {
-      pullDownRadioButtons1.setAttribute("style", "display:none;")
-    }
+  console.log(placeBtns);
+  const unko = placeBtns.childNodes
+  console.log(unko)
+  unko.forEach(function (placeBtn) {
+    placeBtn.addEventListener ("click", () => {
+      if (placeBtn.getAttribute("style") == "background-color:#FFBEDA;") {
+        placeBtn.removeAttribute("style", "background-color:#FFBEDA;")
+      }else {
+        placeBtn.setAttribute("style", "background-color:#FFBEDA;")
+      }
+    });
   });
-})
+
+
+
+  // こだわり検索の表示・非表示
+  detailBtns.forEach(function (detailBtn) {
+    detailBtn.addEventListener ("click", () => {
+      const next = detailBtn.nextElementSibling;
+      if (next.getAttribute("style") == "display:none;") {
+        next.removeAttribute("style", "display:none;")
+      }else {
+        next.setAttribute("style", "display:none;")
+      }
+    });
+  });
+});
