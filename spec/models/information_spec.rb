147cf01e389ef81fcb6b24ab7e054fc40e5dee71
shomahio <75551441+shomahio@users.noreply.github.com>
2021-04-20 22:44:39 +0900

    commit

--- spec/models/information_spec.rb
+++ spec/models/information_spec.rb
@@ -10,14 +10,24 @@ RSpec.describe Information, type: :model do
       it "全ての項目があれば投稿できる" do
         expect(@information).to be_valid
       end
+      
+      it "があれば投稿できる" do
+        @information.building = nil
+        expect(@information).to be_valid
+      end
     end
-    context "料理画像が空では登録できない" do
-      it "画像が空では投稿できない" do
+    context "店舗情報の保存ができない場合" do
+      it "料理画像が空では登録できない" do
+        @information.image = nil
+        @information.valid?
+        expect(@information.errors.full_messages).to include("トップ画像を登録してください")
+      end
+      it "料理画像が空では登録できない" do
         @information.image_foods = nil
         @information.valid?
         expect(@information.errors.full_messages).to include("料理画像は3枚にしてください")
       end
-      it "画像が３枚以上では投稿できない" do
+      it "画像が３枚以下では投稿できない" do
         @information.image_foods.delete_at(1) 
         @information.valid?
         expect(@information.errors.full_messages).to include("料理画像は3枚にしてください")
@@ -26,6 +36,22 @@ RSpec.describe Information, type: :model do
         @information.image_foods << {io: File.open('public/images/test_top.jpg'), filename: 'test_top.jpg'}
         @information.valid?
         expect(@information.errors.full_messages).to include("料理画像は3枚にしてください")
+      end
+      
+      it "外観画像が空では登録できない" do
+        @information.image_appearances = nil
+        @information.valid?
+        expect(@information.errors.full_messages).to include("外観画像は3枚にしてください")
+      end
+      it "画像が３枚以下では投稿できない" do
+        @information.image_appearances.delete_at(1) 
+        @information.valid?
+        expect(@information.errors.full_messages).to include("外観画像は3枚にしてください")
+      end
+      it "画像が３枚以上では投稿できない" do
+        @information.image_appearances << {io: File.open('public/images/test_top.jpg'), filename: 'test_top.jpg'}
+        @information.valid?
+        expect(@information.errors.full_messages).to include("外観画像は3枚にしてください")
       end
     end
   end
