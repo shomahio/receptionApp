711e965c37fed5b21b50ef0d8e2968aa746c9351
shomahio <75551441+shomahio@users.noreply.github.com>
2021-04-23 19:18:45 +0900

    ツイート、詳細ページのビュー

---
+++ spec/factories/receipts.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :receipt do
+    name         {"あああああ"}
+    price     {1000}
+    proviso           { '飲食代' }
+    number    {1111111}
+    consent   {1}
+    association :reservation
+    user { reservation.user }
+    information { reservation.information }
+  end
+end
